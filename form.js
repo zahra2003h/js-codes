751b82f1b1b062fff978f1918b5c6dfc89f4fe44
zahra2003h <154594950+zahra2003h@users.noreply.github.com>
2024-02-07 02:04:34 +0000

    commit 4

--- form.js
+++ form.js
@@ -7,5 +7,10 @@ var tasks = [];
 addButton.addEventListener('click',addText);
 
 function addText() {
-    
+    if (todoField.value == "","  ") {
+        tasks.push(todoField.value);
+    }
+
+   var printText = document.getElementById("pTag").innerHTML= tasks;
 }
+
