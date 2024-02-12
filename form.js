a309d731c14bb3b73b9dc59d9fad374cd7b2edde
zahra2003h <154594950+zahra2003h@users.noreply.github.com>
2024-02-12 19:44:26 +0000

    Fix File Errors

--- form.js
+++ form.js
@@ -2,6 +2,8 @@ const todoField = document.getElementById("todo-field");
 
 const addButton = document.getElementById("add-button");
 
+const ulElement = document.getElementById("list");
+
 var tasks = [];
 
 addButton.addEventListener('click',addText);
@@ -10,7 +12,7 @@ function addText() {
     if (todoField.value == "","  ") {
         tasks.push(todoField.value);
     }
-
-    document.getElementById("pTag").innerHTML= tasks.join("<br>");
-
+    let li = document.createElement("li");
+    li.innerHTML = todoField.value;
+    ulElement.appendChild(li);
 }
